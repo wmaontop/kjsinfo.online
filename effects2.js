0136aa66f511aee222372a86a10ce7ffd3745cfd
WMA <107967606+wmaontop@users.noreply.github.com>
2025-10-25 13:35:02 -0400

    Add files via upload

---
+++ effects2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgSrc = "https://lanyard.cnrad.dev/api/1419122791596163142?bg=00000000&animated=true=true&hideSpotify=false&showDisplayName=true&hideActivity=true";
+    const imgElement = document.getElementById('rpc');
+    imgElement.src = imgSrc;
+});
